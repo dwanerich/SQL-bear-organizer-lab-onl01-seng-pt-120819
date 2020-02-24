bd4cfebe2ba7c105b72f203baa6be237782ffcc5
dwane richards <dwanerich@gmail.com>
2020-02-24 03:34:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,10 +20,14 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) FROM bears GROUP BY color;"
+  
+  
 end
 
 def counts_number_of_bears_with_goofy_temperaments
   "COUNT FROM bears WHERE temperament FROM bears = 'goofy';"
+  
+  
 end
 
 def selects_bear_that_killed_Tim
